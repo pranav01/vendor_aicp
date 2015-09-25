fcf26dbee15785aaf724df18472a1a2f58d0655b
Pranav Reddy <pranavreddy89@yahoo.in>
2015-09-25 09:23:39 +0530

    Update sprout4.mk

--- products/sprout4.mk
+++ products/sprout4.mk
@@ -3,6 +3,12 @@ $(call inherit-product, vendor/aicp/configs/common.mk)
 
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
+# Use Optimizations?
+USE_OPTIMIZATIONS := true
+
+# Enable this if this is a very very old device
+USE_LEGACY_GCC := false
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
 
 PRODUCT_NAME := aicp_sprout4
@@ -13,10 +19,6 @@ PRODUCT_MODEL := A1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="sprout4"
-
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Parth B (TheStrix)"
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 854
